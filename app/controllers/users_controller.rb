fe29736db432d57c5d4f52430e148b49beec2add
Preston Johnson <fromhereonout@gmail.com>
2014-10-01 14:40:01 -0600

    prestonController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
-class UsersController < ApplicationController
+class ProfileController < Devise::DeviseController
   def create
   end
 
   def read
+    @users = @q.result
+    @q = User.search(params[:q])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'index/contact'
-
   root 'index#home'
 
-  devise_for :users
+  get 'index/contact'
+
+  devise_for :users controllers: {}
 
   get 'games/create'
 
@@ -14,14 +14,10 @@ Rails.application.routes.draw do
 
   get 'games/destroy'
 
-  get 'users/create'
-
-  get 'users/read'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
+  get 'profile/create'
+  get 'profile/read'
+  get 'profile/update'
+  get 'profile/destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
