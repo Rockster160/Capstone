5eddc22ab152767c9713e828ff01851252d5141a
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-03 18:10:37 -0600

    Commented out search files until fix.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_username(params[:id])
-    @q = User.search(params[:q])
-    @users = @q.result
+    # @user = User.find_by_username(params[:id])
+    # @q = User.search(params[:q])
+    # @users = @q.result
   end
 end
