3e195f2407ff67a3c4d570d88f64ae86f3a6a690
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-08 11:59:16 -0600

    Finished search listing page- made it adjustable.

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -7,23 +7,25 @@ class SearchResultsController < ApplicationController
     @listgame = []
     @temp = []
     @fwd = params[:q].downcase.chars.sort { |a, b| a.casecmp(b) }.join
-    len_of_str = (Float(@fwd.length)/1.5).ceil
+    @len_of_str = (Float(@fwd.length)/1.5).ceil
     User.all.each do |f|
       count = 0
-      @temp << f.username.downcase.chars.sort { |a, b| a.casecmp(b) }.join
+      # @temp << f.username.downcase.chars.sort { |a, b| a.casecmp(b) }.join
       @fwd.length.times do |pfwd|
-        count += 1 if f.username.include?(@fwd[pfwd])
+        count += 1 if f.username.downcase.include?(@fwd[pfwd])
       end
-      @listuser << f.username if count >= len_of_str
+      @listuser << [f.id, count] if count >= @len_of_str
     end
     Game.all.each do |f|
       count = 0
-      @temp << f.name.downcase.chars.sort { |a, b| a.casecmp(b) }.join
+      # @temp << f.name.downcase.chars.sort { |a, b| a.casecmp(b) }.join
       @fwd.length.times do |pfwd|
-        count += 1 if f.name.include?(@fwd[pfwd])
+        count += 1 if f.name.downcase.include?(@fwd[pfwd])
       end
-      @listgame << f.name if count >= len_of_str
+      @listgame << [f.id, count] if count >= @len_of_str
     end
-    @fwd = len_of_str
+    @fwd = @len_of_str
+    @listgame = @listgame.sort_by{|x,y|y}.reverse
+    @listuser = @listuser.sort_by{|x,y|y}.reverse
   end
 end
