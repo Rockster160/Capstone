7f93f4430ee491c4854684bd3d01de3ca4e0b9ac
Preston Johnson <fromhereonout@gmail.com>
2014-10-09 23:53:04 -0600

    merit sucks

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -17,94 +17,94 @@ Merit.setup do |config|
   # to retrieve :user_model_name object if no `:to` option is given. Default
   # is "current_#{user_model_name.downcase}".
   # config.current_user_method = 'current_user'
+
 end
+  # Create application badges (uses https://github.com/norman/ambry)
+  # badge_id = 0
+  # [{
+  #   id: (badge_id = badge_id+1),
+  #   name: 'just-registered'
+  # }, {
+  #   id: (badge_id = badge_id+1),
+  #   name: 'best-unicorn',
+  #   custom_fields: { category: 'fantasy' }
+  # }].each do |attrs|
+  #   Merit::Badge.create! attrs
+  # end
 
-Merit::Badge.create! (
-  id: 0,
-  name: "first-game-completed",
-  description: "You completed your first game!",
-  custom_fields: { difficulty: :easy }
-)
+  Merit::Badge.create! (
+    id: 0,
+    name: "first-game-completed",
+    description: "You completed your first game!",
+    custom_fields: { difficulty: :easy }
+  )
 
-Merit::Badge.create! (
-  id: 1,
-  name: "personal-high-score-reached",
-  description: "You set your personal high score!",
-  custom_fields: { difficulty: :medium }
-)
+  Merit::Badge.create! (
+    id: 1,
+    name: "personal-high-score-reached",
+    description: "You set your personal high score!",
+    custom_fields: { difficulty: :medium }
+  )
 
-Merit::Badge.create! (
-  id: 2,
-  name: "daily-high-score-reached",
-  description: "You have the daily high score for this game!",
-  custom_fields: { difficulty: :hard }
-)
+  Merit::Badge.create! (
+    id: 2,
+    name: "daily-high-score-reached",
+    description: "You have the daily high score for this game!",
+    custom_fields: { difficulty: :hard }
+  )
 
-Merit::Badge.create! (
-  id: 3,
-  name: "weekly-high-score-reached",
-  description: "You have the weekly high score for this game!",
-  custom_fields: { difficulty: :hard }
-)
+  Merit::Badge.create! (
+    id: 3,
+    name: "weekly-high-score-reached",
+    description: "You have the weekly high score for this game!",
+    custom_fields: { difficulty: :hard }
+  )
 
-Merit::Badge.create! (
-  id: 4,
-  name: "monthly-high-score-reached",
-  description: "You have the monthly high score for this game!",
-  custom_fields: { difficulty: :hard }
-)
+  Merit::Badge.create! (
+    id: 4,
+    name: "monthly-high-score-reached",
+    description: "You have the monthly high score for this game!",
+    custom_fields: { difficulty: :hard }
+  )
 
-Merit::Badge.create! (
-  id: 5,
-  name: "first-level-completed",
-  description: "You completed the first level of this game!",
-  custom_fields: { difficulty: :easy }
-)
+  Merit::Badge.create! (
+    id: 5,
+    name: "first-level-completed",
+    description: "You completed the first level of this game!",
+    custom_fields: { difficulty: :easy }
+  )
 
-Merit::Badge.create! (
-  id: 6,
-  name: "first-five-levels-completed",
-  description: "You completed the first five levels of this game!",
-  custom_fields: { difficulty: :medium }
-)
+  Merit::Badge.create! (
+    id: 6,
+    name: "first-five-levels-completed",
+    description: "You completed the first five levels of this game!",
+    custom_fields: { difficulty: :medium }
+  )
 
-Merit::Badge.create! (
-  id: 7,
-  name: "all-levels-completed",
-  description: "You completed all the levels of this game!",
-  custom_fields: { difficulty: :hard }
-)
+  Merit::Badge.create! (
+    id: 7,
+    name: "all-levels-completed",
+    description: "You completed all the levels of this game!",
+    custom_fields: { difficulty: :hard }
+  )
 
-Merit::Badge.create! (
-  id: 8,
-  name: "first-checkpoint-reached",
-  description: "You reached the first checkpoint!",
-  custom_fields: { difficulty: :easy }
-)
+  Merit::Badge.create! (
+    id: 8,
+    name: "first-checkpoint-reached",
+    description: "You reached the first checkpoint!",
+    custom_fields: { difficulty: :easy }
+  )
 
-Merit::Badge.create! (
-  id: 9,
-  name: "second-checkpoint-reached",
-  description: "You reached the second checkpoint!",
-  custom_fields: { difficulty: :medium }
-)
+  Merit::Badge.create! (
+    id: 9,
+    name: "second-checkpoint-reached",
+    description: "You reached the second checkpoint!",
+    custom_fields: { difficulty: :medium }
+  )
 
-Merit::Badge.create! (
-  id: 10,
-  name: "game-completed",
-  description: "You beat the game!",
-  custom_fields: { difficulty: :hard }
-)
-
-# Create application badges (uses https://github.com/norman/ambry)
-# badge_id = 0
-# [{
-#   id: (badge_id = badge_id+1),
-#   name: 'just-registered'
-# }, {
-#   id: (badge_id = badge_id+1),
-#   name: 'best-unicorn',
-#   custom_fields: { category: 'fantasy' }
-# }].each do |attrs|
-#   Merit::Badge.create! attrs
-# end
+  Merit::Badge.create! (
+    id: 10,
+    name: "game-completed",
+    description: "You beat the game!",
+    custom_fields: { difficulty: :hard }
+  )
