1e0d424d513704624810592a76024125d78c551a
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-21 16:07:28 -0600

    Schema file made from migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -97,12 +97,12 @@ ActiveRecord::Schema.define(version: 20141021170437) do
     t.integer  "coin",                   default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",           null: false
+    t.string   "encrypted_password",     default: "",           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -113,8 +113,8 @@ ActiveRecord::Schema.define(version: 20141021170437) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer  "coinTo",                 default: 20
-    t.integer  "favorites",              default: [],              array: true
-    t.integer  "popup",                  default: [],              array: true
+    t.integer  "favorites",              default: [0, 0, 0, 0],              array: true
+    t.integer  "popup",                  default: [],                        array: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
