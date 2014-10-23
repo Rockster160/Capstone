1fdb2912ebfd5d6bb77c72f3e91bbecf4475219a
erikb <erikburton1@gmail.com>
2014-10-22 18:45:46 -0600

    added function for links on profile page where users can add their facebook, twitter and personal website URLs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021170437) do
+ActiveRecord::Schema.define(version: 20141023000120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,9 @@ ActiveRecord::Schema.define(version: 20141021170437) do
     t.integer  "coinTo",                 default: 20
     t.integer  "favorites",              default: [0, 0, 0, 0],              array: true
     t.integer  "popup",                  default: [],                        array: true
+    t.string   "facebook_url"
+    t.string   "twitter_url"
+    t.string   "website_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
