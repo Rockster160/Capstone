0096329507fd3e30e10329fddebc65f24f9fd561
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-24 12:52:51 -0600

    Added Standard Deviation to RNG in Game play mode.

--- app/assets/javascripts/scoremiddleman.js
+++ app/assets/javascripts/scoremiddleman.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
-
   $('#pullscore').click(function(){
-    $('#cheatinput').val("");
-    // alert(score_low + ", " + score_high);
-    if (score_high > score_low) {
-      var mid = score_high / 2;
-      rng = (Math.random() * mid) + mid /2;
-    };
-    if (score_high < score_low) {
-      rng = (Math.random() * score_low) + score_high;
-    };
-    console.log(rng);
-    $('#cheatinput').val();
+    if ($('#cheatinput').val().length == 0) {
+      // alert(score_low + ", " + score_high);
+      if (score_high < score_low) {
+        var holdhigh = score_high;
+        var holdlow = score_low;
+        score_high = holdlow;
+        score_low = holdhigh;
+      };
+      if (score_high > score_low) {
+        var mid = score_high / 2;
+        rng = ((Math.random() * score_high) + (Math.random() * score_high) + (Math.random() * score_high)) / 3;
+        rng = Math.round(rng);
+        grab = Math.round((Math.random() * score_high) + score_low);
+        if (rng == grab) {
+          rng = score_high;
+          console.log("They match!! It happened!!");
+        };
+      } else {
+        console.log("Error! High is lower than low...")
+      };
+      // console.log(rng + ", " + grab);
+      $('#cheatinput').val(rng);
+    }
     var gamescore = $('#cheatinput').val()
     url = window.location.href;
     if (gamescore.length > 0) {
-      $.get(url, {passScore: gamescore}, function(searchResults) {
-        window.location = "/"
+      $.get(url, {passScore: gamescore}, function() {
+        window.location = "/";
       })
     }
   });
