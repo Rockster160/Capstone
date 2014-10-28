577a80d2d560c7bce2621c913366fb3be10274fb
Preston Johnson <fromhereonout@gmail.com>
2014-10-28 14:12:26 -0600

    trophy stuff

--- db/schema.rb
+++ db/schema.rb
@@ -113,8 +113,7 @@ ActiveRecord::Schema.define(version: 20141025210707) do
     t.string   "twitter_url",            default: ""
     t.string   "website_url",            default: ""
     t.string   "ava"
-    t.datetime "last_in",                default: '2014-10-28 00:39:22'
-
+    t.datetime "last_in"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
