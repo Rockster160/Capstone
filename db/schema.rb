a8eb0662a6f5eda66800076fa01d548e0967e8c2
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-27 18:57:02 -0600

    Schema fixed.

--- db/schema.rb
+++ db/schema.rb
@@ -95,20 +95,20 @@ ActiveRecord::Schema.define(version: 20141025210707) do
     t.integer  "coin",                   default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "",                    null: false
-    t.string   "encrypted_password",     default: "",                    null: false
+    t.string   "email",                  default: "",           null: false
+    t.string   "encrypted_password",     default: "",           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                     null: false
+    t.integer  "sign_in_count",          default: 0,            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.string   "username"
     t.integer  "coinTo",                 default: 1000
-    t.integer  "favorites",              default: [0, 0, 0, 0],                       array: true
-    t.integer  "popup",                  default: [],                                 array: true
+    t.integer  "favorites",              default: [0, 0, 0, 0],              array: true
+    t.integer  "popup",                  default: [],                        array: true
     t.string   "facebook_url",           default: ""
     t.string   "twitter_url",            default: ""
     t.string   "website_url",            default: ""
