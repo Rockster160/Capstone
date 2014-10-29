6f638e9032758c7f6e9351dd7a284d9a074477fc
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-10-29 11:42:05 -0600

    Made Play always do a random number unless clicking on the 'secret' button.

--- app/assets/javascripts/scoremiddleman.js
+++ app/assets/javascripts/scoremiddleman.js
@@ -1,35 +1,34 @@
 $(document).ready(function(){
   $('#pullscore').click(function(){
-    if ($('#cheatinput').val().length == 0) {
-      // alert(score_low + ", " + score_high);
-      if (score_high < score_low) {
-        var holdhigh = score_high;
-        var holdlow = score_low;
-        score_high = holdlow;
-        score_low = holdhigh;
-      };
-      if (score_high > score_low) {
-        var mid = score_high / 2;
-        rng = ((Math.random() * score_high) + (Math.random() * score_high) + (Math.random() * score_high)) / 3;
-        rng = Math.round(rng);
-        grab = Math.round((Math.random() * score_high) + score_low);
-        if (rng == grab) {
-          rng = score_high;
-          console.log("They match!! It happened!!");
-        };
-      } else {
-        console.log("Error! High is lower than low...")
-      };
-      // console.log(rng + ", " + grab);
-      $('#cheatinput').val(rng);
-    }
-    var gamescore = $('#cheatinput').val()
-    url = window.location.href;
-    if (gamescore.length > 0) {
-      $.get(url, {passScore: gamescore}, function() {
-        window.location = "/";
-      })
-    }
+    scorePull(-1);
   });
+  $('#cheatbtn').click(function(){
+    scorePull($('#cheatinput').val());
+  })
 
 })
+
+function scorePull(x) {
+  if (x == -1) {
+    if (score_high > score_low) {
+      var mid = score_high / 2;
+      rng = ((Math.random() * score_high) + (Math.random() * score_high) + (Math.random() * score_high)) / 3;
+      rng = Math.round(rng);
+      grab = Math.round((Math.random() * score_high) + score_low);
+      if (rng == grab) {
+        rng = score_high;
+        console.log("They match!! It happened!!");
+      };
+    } else {
+      console.log("Error! High is lower than low...")
+    };
+    $('#cheatinput').val(rng);
+  }
+  var gamescore = $('#cheatinput').val()
+  url = window.location.href;
+  if (gamescore.length > 0) {
+    $.get(url, {passScore: gamescore}, function() {
+      window.location = "/";
+    })
+  }
+}
